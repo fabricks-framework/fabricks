cef340c6c5eb03625af5daf80ef956686607bdd2
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2024-08-01 14:10:54 +0200

    next fix

--- framework/tests/initialize.py
+++ framework/tests/initialize.py
@@ -6,7 +6,7 @@ import sys
 from fabricks.context import IS_TEST, PATH_RUNTIME
 from fabricks.context.log import Logger
 from fabricks.metastore.database import Database
-framework_path = str((PATH_RUNTIME.parent.parent).absolute())
+framework_path = str(PATH_RUNTIME.pathlib.parent.parent.absolute())
 print(framework_path)
 sys.path.append(framework_path)
 from tests.types import paths
