8e724bf17566f062e7bfbcab8bc421185383b696
Poskin Francois <Francois.Poskin@bmsuisse.ch>
2024-09-12 17:09:12 +0200

    fmt

--- framework/tests/integration/runtime/gold/gold/dim/time.sql
+++ framework/tests/integration/runtime/gold/gold/dim/time.sql
@@ -1,7 +1,11 @@
 with
     h as (select explode(sequence(0, 23, 1)) as h),
     m as (select explode(sequence(0, 59, 1)) as m),
-    merger as (select format_number(h, '00') as hour, format_number(m, '00') as minute from h cross join m)
+    merger as (
+        select format_number(h, '00') as hour, format_number(m, '00') as minute
+        from h
+        cross join m
+    )
 select
     cast(concat(hour, minute) as int) as __identity,
     concat(hour, minute) as __key,
