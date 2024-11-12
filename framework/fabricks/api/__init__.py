d4118dd80a15eaf3a8cf806373f5a8b51fa589e0
Poskin Francois <Francois.Poskin@bmsuisse.ch>
2024-11-12 11:03:41 +0100

    fmt

--- framework/fabricks/api/__init__.py
+++ framework/fabricks/api/__init__.py
@@ -1,5 +1,5 @@
-from fabricks.api.core import get_job, get_jobs, get_step
 from fabricks.api.context import init_spark_session
+from fabricks.api.core import get_job, get_jobs, get_step
 
 __all__ = [
     "init_spark_session",
