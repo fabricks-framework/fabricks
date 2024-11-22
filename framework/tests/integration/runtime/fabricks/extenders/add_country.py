50636538c10edbc92f4f219635d2dc1ca0a526a6
poskinf <poskinf>
2024-11-22 14:10:36 +0000

    wip

--- framework/tests/integration/runtime/fabricks/extenders/add_country.py
+++ framework/tests/integration/runtime/fabricks/extenders/add_country.py
@@ -6,5 +6,5 @@ from fabricks.core.extenders import extender
 
 @extender(name="add_country")
 def add_country(df: DataFrame, **kwargs) -> DataFrame:
-    df = df.withColumn("country", lit(kwrags.get("country")))
+    df = df.withColumn("country", lit(kwargs.get("country")))
     return df
