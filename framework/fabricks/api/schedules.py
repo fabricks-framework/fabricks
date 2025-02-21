2774b812f638804c443ae2b94c4584daf31f6d3d
Poskin Francois <Francois.Poskin@bmsuisse.ch>
2025-02-21 16:15:04 +0100

    wip

--- framework/fabricks/api/schedules.py
+++ framework/fabricks/api/schedules.py
@@ -1,8 +1,17 @@
-from fabricks.core.schedules import create_or_replace_view, create_or_replace_views, get_schedule, get_schedules
+from fabricks.core.schedules import (
+    create_or_replace_view,
+    create_or_replace_views,
+    get_dependencies,
+    get_mermaid_diagram,
+    get_schedule,
+    get_schedules,
+)
 
 __all__ = [
-    "get_schedules",
-    "get_schedule",
     "create_or_replace_view",
     "create_or_replace_views",
+    "get_dependencies",
+    "get_mermaid_diagram",
+    "get_schedule",
+    "get_schedules",
 ]
--- framework/fabricks/core/schedules.py
+++ framework/fabricks/core/schedules.py
@@ -86,3 +86,48 @@ def create_or_replace_views():
             _create_or_replace_view(row.name, row.options)
         except Exception:
             Logger.exception(f"schedule - {row.name} not created nor replaced")
+
+
+def get_dependencies(name: str) -> DataFrame:
+    from fabricks.core.dags import DagGenerator
+
+    g = DagGenerator(schedule=name)
+    return g.get_dependencies()
+
+
+def get_mermaid_diagram(name: str) -> str:
+    df = get_dependencies(name)
+
+    df = df.withColumnRenamed("ParentId", "parent_id")
+    df = df.withColumnRenamed("Parent", "parent")
+    df = df.withColumnRenamed("JobId", "job_id")
+    df = df.withColumnRenamed("Job", "job")
+
+    dependencies = df.select("parent_id", "parent", "job_id", "job").collect()
+
+    out = "flowchart TD\n"
+
+    unique_nodes = set()
+
+    for row in dependencies:
+        parent_id = str(row["parent_id"])
+        parent_name = str(row["parent"])
+        child_id = str(row["job_id"])
+        child_name = str(row["job"])
+
+        if parent_id != "0" and parent_id is not None:
+            if parent_id not in unique_nodes:
+                out += f"    {parent_id}[{parent_name}]\n"
+                unique_nodes.add(parent_id)
+
+            if child_id not in unique_nodes:
+                out += f"    {child_id}[{child_name}]\n"
+                unique_nodes.add(child_id)
+
+            out += f"    {parent_id} --> {child_id}\n"
+        else:
+            if child_id not in unique_nodes:
+                out += f"    {child_id}[{child_name}]\n"
+                unique_nodes.add(child_id)
+
+    return out
