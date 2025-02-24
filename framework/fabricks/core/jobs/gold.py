9246b5376ae136c698e8383d47815bf61235bc33
Poskin Francois <Francois.Poskin@bmsuisse.ch>
2025-02-24 09:01:13 +0100

    fmt

--- framework/fabricks/core/jobs/gold.py
+++ framework/fabricks/core/jobs/gold.py
@@ -255,11 +255,11 @@ class Gold(BaseJob):
         assert self.change_data_capture in ["scd1", "scd2"], "persist_last_timestamp only allowed in scd1 or scd2"
 
         cdc = NoCDC(self.step, self.topic, f"{self.item}__last_timestamp")
-        
+
         if drop:
             cdc.drop()
             return
-        
+
         if get:
             return self.table.get_last_version()
 
