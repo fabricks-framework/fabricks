8041e5e0d675a8378f3004efbcfe3bb85a844f9a
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2025-04-17 15:09:22 +0200

    remove deprecated function

--- framework/fabricks/core/extenders.py
+++ framework/fabricks/core/extenders.py
@@ -1,22 +1,21 @@
 import sys
-from importlib.util import spec_from_file_location
+from importlib.util import spec_from_file_location, module_from_spec
 from typing import Callable
 
-from fabricks.context import PATH_EXTENDERS
+from fabricks.context import PATH_EXTENDERS, PATH_RUNTIME
 
 EXTENDERS: dict[str, Callable] = {}
 
 
 def get_extender(name: str) -> Callable:
-    sys.path.append(PATH_EXTENDERS.string)
-
-    path = PATH_EXTENDERS.join(f"{name}.py")
+    path = PATH_RUNTIME.join(PATH_EXTENDERS.join(f"{name}.py"))
     assert path.exists(), "no valid extender found in {path.string}"
 
     spec = spec_from_file_location(name, path.string)
     assert spec, "no valid extender found in {path.string}"
-
-    spec.loader.load_module()  # type: ignore
+    assert spec.loader is not None
+    mod = module_from_spec(spec)
+    spec.loader.exec_module(mod)
     e = EXTENDERS[name]
     return e
 
