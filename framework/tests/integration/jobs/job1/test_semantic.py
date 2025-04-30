b0d32f000b201bc246645836fa7b26c0f64845b9
francois.poskin@bmsuisse.ch <francois.poskin@bmsuisse.ch>
2025-04-30 09:23:12 +0000

    2 not working

--- framework/tests/integration/jobs/job1/test_semantic.py
+++ framework/tests/integration/jobs/job1/test_semantic.py
@@ -25,8 +25,8 @@ def test_semantic_fact_step_option():
     assert props_df.where("key == 'delta.minReaderVersion'").select("value").collect()[0][0] == "1", (
         "minReaderVersion <> 1"
     )
-    assert props_df.where("key == 'delta.minWriterVersion'").select("value").collect()[0][0] == "2", (
-        "minWriterVersion <> 2"
+    assert props_df.where("key == 'delta.minWriterVersion'").select("value").collect()[0][0] == "7", (
+        "minWriterVersion <> 7"
     )
     assert props_df.where("key == 'delta.targetFileSize'").select("value").collect()[0][0] == "1073741824", (
         "delta.targetFileSize <> 1073741824"
