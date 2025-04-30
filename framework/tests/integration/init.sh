1296eb6c6830dcc3111cd488bcf9f0c693e70875
Poskin Francois <Francois.Poskin@bmsuisse.ch>
2025-04-30 12:56:31 +0200

    add tqdm

--- framework/tests/integration/init.sh
+++ framework/tests/integration/init.sh
@@ -7,7 +7,6 @@ sudo echo FABRICKS_RUNTIME=$PARENT/runtime >> /etc/environment
 sudo echo FABRICKS_NOTEBOOKS=$GRANDPARENT/fabricks/api/notebooks >> /etc/environment
 sudo echo FABRICKS_CONFIG=/fabricks/conf.fabricks.yml >> /etc/environment
 
-sudo echo FABRICKS_IS_TEST=TRUE >> /etc/environment
 sudo echo FABRICKS_IS_JOB_CONFIG_FROM_YAML=TRUE >> /etc/environment
 
-/databricks/python/bin/pip install pytest pytest-order pyyaml sqlglot jinja2 polars azure-data-tables azure-storage-queue azure-identity python-dotenv coloroma
+/databricks/python/bin/pip install pytest pytest-order pyyaml sqlglot jinja2 polars azure-data-tables azure-storage-queue azure-identity python-dotenv coloroma tqdm
