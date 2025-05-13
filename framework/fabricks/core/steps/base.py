b58704ddb34344d973e6a726f259c8bf7aa7e021
Poskin Francois <Francois.Poskin@bmsuisse.ch>
2025-05-13 11:46:42 +0200

    do not return a dataframe if the update dependencies fails

--- framework/fabricks/core/steps/base.py
+++ framework/fabricks/core/steps/base.py
@@ -8,7 +8,7 @@ from pyspark.sql.types import Row
 from fabricks.cdc import SCD1
 from fabricks.context import CONF_RUNTIME, LOGLEVEL, PATHS_RUNTIME, PATHS_STORAGE, SPARK, STEPS
 from fabricks.context.log import DEFAULT_LOGGER
-from fabricks.core.jobs.base._types import Bronzes, Golds, SchemaDependencies, Silvers, TStep
+from fabricks.core.jobs.base._types import Bronzes, Golds, Silvers, TStep
 from fabricks.core.jobs.get_job import get_job
 from fabricks.core.steps._types import Timeouts
 from fabricks.core.steps.get_step_conf import get_step_conf
@@ -149,11 +149,10 @@ class BaseStep:
         self,
         progress_bar: Optional[bool] = False,
         topic: Optional[Union[str, List[str]]] = None,
-    ) -> DataFrame:
+    ) -> Optional[DataFrame]:
         DEFAULT_LOGGER.debug("get dependencies", extra={"step": self})
 
         errors = []
-        df = None
 
         def _get_dependencies(row: Row):
             job = get_job(step=self.name, job_id=row["job_id"])
@@ -185,12 +184,7 @@ class BaseStep:
             for e in errors:
                 DEFAULT_LOGGER.error("failed to get dependencies", extra={"step": e})
 
-            df = concat_dfs(dfs)
-
-        if df is not None:
-            return df
-        else:
-            return SPARK.createDataFrame([], schema=SchemaDependencies)
+            return concat_dfs(dfs)
 
     def get_jobs_iter(self, topic: Optional[str] = None):
         return read_yaml(self.runtime, root="job", prio_file_name=topic)
@@ -299,6 +293,7 @@ class BaseStep:
 
             if topic is None:
                 SCD1("fabricks", self.name, "dependencies").delete_missing(df, keys=["dependency_id"])
+                
             else:
                 if isinstance(topic, str):
                     topic = [topic]
