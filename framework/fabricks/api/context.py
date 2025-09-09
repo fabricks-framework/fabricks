852fabe48b05c4250d2ed62323abe4874acd283d
fpo <francois.poskin@bmsuisse.ch>
2025-09-09 15:38:16 +0200

    add function to api

--- framework/fabricks/api/context.py
+++ framework/fabricks/api/context.py
@@ -1,4 +1,5 @@
 from fabricks.context import BRONZE, DBUTILS, GOLD, SECRET_SCOPE, SILVER, SPARK, init_spark_session
+from fabricks.context.runtime import pprint_runtime
 from fabricks.core.jobs.base._types import Bronzes, Golds, Silvers, Steps
 
 # step
@@ -17,6 +18,7 @@ __all__ = [
     "Golds",
     "GOLDS",
     "init_spark_session",
+    "pprint_runtime",
     "SECRET_SCOPE",
     "SILVER",
     "Silvers",
