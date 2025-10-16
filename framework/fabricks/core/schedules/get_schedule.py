f200bb9bd0526d26a76462ebbdba4c47f9ab3936
fpo <francois.poskin@bmsuisse.ch>
2025-10-16 16:08:49 +0200

    why ???

--- framework/fabricks/core/schedules/get_schedule.py
+++ framework/fabricks/core/schedules/get_schedule.py
@@ -1,7 +1,5 @@
-from fabricks.core.jobs.get_schedule import get_schedule  # avoid circular import
-from fabricks.core.jobs.get_schedules import get_schedules  # void circular import
+from fabricks.core.jobs.get_schedule import get_schedule  # void circular import
 
 __all__ = [
-    "get_schedules",
     "get_schedule",
 ]
