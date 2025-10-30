6c1aa9929616dc19ace589c8ebea99a2dd90c4eb
francois.poskin@bmsuisse.ch <francois.poskin@bmsuisse.ch>
2025-10-30 15:48:03 +0000

    bugfix gold invoker

--- framework/fabricks/core/jobs/gold.py
+++ framework/fabricks/core/jobs/gold.py
@@ -123,7 +123,7 @@ class Gold(BaseJob):
 
         elif self.options.job.get("notebook"):
             invokers = self.options.invokers.get_list("run")
-            assert len(invokers) == 1, "only one invoker allowed when notebook is true"
+            assert len(invokers) <= 1, "at most one invoker allowed when notebook is true"
 
             global_temp_view = self.invoke(path=self.paths.runtime, schema_only=schema_only, **kwargs)
             assert global_temp_view is not None, "global_temp_view not found"
