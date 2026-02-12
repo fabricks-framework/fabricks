257f7090a27d1977825ddebfd5bee78f07c95f82
François Poskin <francois.poskin@bmsuisse.ch>
2026-02-12 11:08:52 +0100

    make run more flexible

--- framework/fabricks/api/notebooks/run.py
+++ framework/fabricks/api/notebooks/run.py
@@ -3,7 +3,6 @@
 
 # COMMAND ----------
 
-import json
 
 from databricks.sdk.runtime import dbutils
 
@@ -13,9 +12,6 @@ from fabricks.core.schedules import run
 
 dbutils.widgets.text("step", "---")
 dbutils.widgets.text("job_id", "---")
-dbutils.widgets.text("job", "--")
-dbutils.widgets.text("schedule_id", "---")
-dbutils.widgets.text("schedule", "---")
 
 # COMMAND ----------
 
@@ -29,30 +25,7 @@ assert job_id != "---"
 
 # COMMAND ----------
 
-job = dbutils.widgets.get("job")
-assert job != "---"
-
-# COMMAND ----------
-
-schedule_id = dbutils.widgets.get("schedule_id")
-assert schedule_id != "---"
-
-# COMMAND ----------
-
-schedule = dbutils.widgets.get("schedule")
-assert schedule != "---"
-
-# COMMAND ----------
-
-try:
-    context = json.loads(dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson())  # type: ignore
-    notebook_id = context.get("tags").get("jobId")
-except:  # noqa: E722
-    notebook_id = None
-
-# COMMAND ----------
-
-run(step=step, job_id=job_id, schedule_id=schedule_id, schedule=schedule, notebook_id=notebook_id)
+run(step=step, job_id=job_id)
 
 # COMMAND ----------
 
--- framework/fabricks/core/dags/run.py
+++ framework/fabricks/core/dags/run.py
@@ -1,27 +1,125 @@
+import json
+from typing import Callable, overload
+
+from databricks.sdk.runtime import dbutils
+
 from fabricks.core.dags.log import LOGGER, TABLE_LOG_HANDLER
-from fabricks.core.jobs import get_job
+from fabricks.core.jobs import Bronze, Gold, Silver, get_job
 from fabricks.core.jobs.base.exception import CheckWarning, SkipWarning
 
 
-def run(step: str, job_id: str, schedule_id: str, schedule: str, notebook_id: str | None = None):
-    job = get_job(step=step, job_id=job_id)
+@overload
+def run(
+    step: str,
+    *,
+    job_id: str,
+    schedule_id: str | None = None,
+    schedule: str | None = None,
+    notebook_id: str | None = None,
+    pre_run_callable: Callable | None = None,
+    post_run_callable: Callable | None = None,
+    data: dict | None = None,
+    **kwargs,
+) -> None: ...
+
+
+@overload
+def run(
+    step: str,
+    topic: str,
+    item: str,
+    *,
+    schedule_id: str | None = None,
+    schedule: str | None = None,
+    notebook_id: str | None = None,
+    pre_run_callable: Callable | None = None,
+    post_run_callable: Callable | None = None,
+    data: dict | None = None,
+    **kwargs,
+) -> None: ...
+
+
+@overload
+def run(
+    *,
+    job: Bronze | Silver | Gold,
+    schedule_id: str | None = None,
+    schedule: str | None = None,
+    notebook_id: str | None = None,
+    pre_run_callable: Callable | None = None,
+    post_run_callable: Callable | None = None,
+    data: dict | None = None,
+    **kwargs,
+) -> None: ...
+
+
+def run(
+    step: str | None = None,
+    topic: str | None = None,
+    item: str | None = None,
+    job_id: str | None = None,
+    job: Bronze | Silver | Gold | None = None,
+    schedule_id: str | None = None,
+    schedule: str | None = None,
+    notebook_id: str | None = None,
+    pre_run_callable: Callable | None = None,
+    post_run_callable: Callable | None = None,
+    data: dict | None = None,
+    **kwargs,
+) -> None:
+    if job is None:
+        if step is not None and job_id is not None:
+            job = get_job(step=step, job_id=job_id)
+        elif step is not None and topic is not None and item is not None:
+            job = get_job(step=step, topic=topic, item=item)
+        else:
+            raise ValueError("either job or step+job_id or step+topic+item must be provided")
+
+    assert job is not None
+
+    if schedule_id is None:
+        try:
+            schedule_id = dbutils.widgets.get("schedule_id")
+        except:  # noqa: E722
+            schedule_id = None
+
+    if schedule is None:
+        try:
+            schedule = dbutils.widgets.get("schedule")
+        except:  # noqa: E722
+            schedule = None
+
+    if notebook_id is None:
+        try:
+            context = json.loads(dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson())  # type: ignore
+            notebook_id = context.get("tags").get("jobId")
+        except:  # noqa: E722
+            notebook_id = None
 
     extra = {
         "partition_key": schedule_id,
         "schedule_id": schedule_id,
         "schedule": schedule,
         "step": step,
-        "job": job,
+        "job": str(job),
         "target": "buffer",
+        "notebook_id": notebook_id,
+        "json": json.dumps(data),
     }
-    if notebook_id is not None:
-        extra["notebook_id"] = notebook_id
 
     LOGGER.info("running", extra=extra)
 
     try:
-        job.run(schedule_id=schedule_id, schedule=schedule)
+        if pre_run_callable is not None:
+            LOGGER.debug("invoke pre-run callable", extra=extra)
+            pre_run_callable(**kwargs)
+
+        job.run(**kwargs)
         LOGGER.info("done", extra=extra)
+
+        if post_run_callable is not None:
+            LOGGER.debug("invoke post-run callable", extra=extra)
+            post_run_callable(**kwargs)
 
     except SkipWarning:
         LOGGER.exception("skipped", extra=extra)
