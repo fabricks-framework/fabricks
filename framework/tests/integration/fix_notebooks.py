4019df4056e1ff9322a565f5ae97fa69db21cd23
francois.poskin@bmsuisse.ch <francois.poskin@bmsuisse.ch>
2026-02-12 14:57:54 +0000

    swithc to python notrebook

---
+++ framework/tests/integration/fix_notebooks.py
@@ -0,0 +1,32 @@
+# Databricks notebook source
+# MAGIC %run ./add_missing_modules
+
+# COMMAND ----------
+
+import os
+
+from fabricks.context import PATH_NOTEBOOKS
+
+# COMMAND ----------
+
+for n in [
+    "cluster",
+    "initialize",
+    "process",
+    "standalone",
+    "run",
+    "terminate",
+]:
+    path = os.path.join(str(PATH_NOTEBOOKS), f"{n}.py")
+    print(path)
+    with open(path, "r") as f:
+        content = f.read()
+
+    if "# MAGIC %run ./add_missing_modules" not in content:
+        content = content.replace(
+            "# Databricks notebook source",
+            "# Databricks notebook source\n# MAGIC %run ./add_missing_modules\n# COMMAND ----------",
+        )
+
+        with open(path, "w") as f:
+            f.write(content)
