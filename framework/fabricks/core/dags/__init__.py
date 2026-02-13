83d5dcf38b5e515274c8faa480e244bfc56bd949
François Poskin <francois.poskin@bmsuisse.ch>
2026-02-13 10:14:39 +0100

    bugfic

--- framework/fabricks/core/dags/__init__.py
+++ framework/fabricks/core/dags/__init__.py
@@ -1,9 +1,11 @@
 from fabricks.core.dags.generator import DagGenerator
 from fabricks.core.dags.processor import DagProcessor
 from fabricks.core.dags.terminator import DagTerminator
+from fabricks.core.dags.run import run
 
 __all__ = [
     "DagGenerator",
     "DagProcessor",
     "DagTerminator",
+    "run",
 ]
