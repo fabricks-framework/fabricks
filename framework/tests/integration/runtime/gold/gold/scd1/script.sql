fae13c96e1a969e9740eb31f0960e18fe8a1adc0
francois.poskin@bmsuisse.ch <francois.poskin@bmsuisse.ch>
2026-02-27 10:26:00 +0000

    make it work

--- framework/tests/integration/runtime/gold/gold/scd1/script.sql
+++ framework/tests/integration/runtime/gold/gold/scd1/script.sql
@@ -1,8 +1,4 @@
-declare or replace _max DATE;
-declare or replace _min DATE;
-set variable _max = (select max(`__valid_from`) from silver.monarch_scd2)
-;
-set variable _min = (select min(`__valid_from`) from silver.monarch_scd2)
-;
+declare variable _max DATE  = (select max(`__valid_from`) from silver.monarch_scd2);
+declare variable _min DATE = (select min(`__valid_from`) from silver.monarch_scd2);
 select `id` as __key, `id` as id, name as monarch, `doubleField` as `value`, _max as `max`, _min as `min`
 from silver.monarch_scd1__current s
