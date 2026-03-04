82d93697ca41d0a7e0599368bdb518e73c2cd252
François Poskin <francois.poskin@bmsuisse.ch>
2026-03-04 14:52:26 +0100

    wip

--- framework/fabricks/core/jobs/base/configurator.py
+++ framework/fabricks/core/jobs/base/configurator.py
@@ -79,6 +79,8 @@ class Configurator(ABC):
     _change_data_capture: Optional[AllowedChangeDataCaptures] = None
     _mode: Optional[AllowedModes] = None
 
+    _udf_registered: Optional[bool] = None
+
     @property
     @abstractmethod
     def stream(self) -> bool: ...
@@ -336,13 +338,16 @@ class Configurator(ABC):
 
             return list(set(udfs))
 
-    def register_udfs(self):
-        udfs = self.get_udfs()
-        if udfs:
-            for u in udfs:
-                if not is_registered(u, self.spark):
-                    DEFAULT_LOGGER.debug(f"register udf {u}", extra={"label": self})
-                    register_udf(u, spark=self.spark)
+    def register_udfs(self, force: bool | None = False):
+        if not self._udf_registered or force:
+            udfs = self.get_udfs()
+            if udfs:
+                for u in udfs:
+                    if not is_registered(u, self.spark):
+                        DEFAULT_LOGGER.debug(f"register udf {u}", extra={"label": self})
+                        register_udf(u, spark=self.spark)
+
+            self._udf_registered = True
 
     def _match_udfs(self, string: str) -> Optional[list[str]]:
         if f"{UDF_PREFIX}" in string:
