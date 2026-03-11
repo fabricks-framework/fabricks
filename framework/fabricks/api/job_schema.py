84fd4c85cfdb6fa1309e657d5b805810a6b082e0
François Poskin <francois.poskin@bmsuisse.ch>
2026-03-11 13:48:24 +0100

    mino

--- framework/fabricks/api/job_schema.py
+++ framework/fabricks/api/job_schema.py
@@ -1,15 +1,15 @@
 from fabricks.core.job_schema import get_job_schema, print_job_schema
 
 job_schema = get_job_schema()
-bronze_job_schema = get_job_schema("bronze")
-silver_job_schema = get_job_schema("silver")
-gold_job_schema = get_job_schema("gold")
+job_schema_bronze = get_job_schema("bronze")
+job_schema_silver = get_job_schema("silver")
+job_schema_gold = get_job_schema("gold")
 
 __all__ = [
     "get_job_schema",
     "print_job_schema",
     "job_schema",
-    "bronze_job_schema",
-    "silver_job_schema",
-    "gold_job_schema",
+    "job_schema_bronze",
+    "job_schema_silver",
+    "job_schema_gold",
 ]
