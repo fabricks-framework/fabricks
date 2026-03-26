568b84e14a83590b1985ba97ed7cc2dd901ff3a6
François Poskin <francois.poskin@bmsuisse.ch>
2026-03-26 11:13:19 +0100

    better

--- framework/tests/integration/runtime/fabricks/extenders/drop__cols.py
+++ framework/tests/integration/runtime/fabricks/extenders/drop__cols.py
@@ -6,5 +6,6 @@ from fabricks.core.extenders import extender
 @extender(name="drop__cols")
 def drop__cols(df: DataFrame) -> DataFrame:
     cols = [c for c in df.columns if c.startswith("__")]
+    cols = [c for c in cols if c not in ["__partition_by_1", "__cluster_by_1"]]
     df = df.drop(*cols)
     return df
