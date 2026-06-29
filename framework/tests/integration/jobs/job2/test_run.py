1e558bd27d8d0a1306ee6cac1f6413e7356e7b15
francois.poskin@bmsuisse.ch <francois.poskin@bmsuisse.ch>
2026-06-29 15:52:17 +0000

    minor

--- framework/tests/integration/jobs/job2/test_run.py
+++ framework/tests/integration/jobs/job2/test_run.py
@@ -2,9 +2,9 @@ from logging import ERROR
 
 import pytest
 
-from fabricks.context import PATH_RUNTIME
 from fabricks.context.log import DEFAULT_LOGGER
 from fabricks.utils.helpers import run_notebook
+from tests.integration._types import paths
 
 DEFAULT_LOGGER.setLevel(ERROR)
 
@@ -12,7 +12,7 @@ DEFAULT_LOGGER.setLevel(ERROR)
 @pytest.mark.order(201)
 def test_run():
     try:
-        run_notebook(PATH_RUNTIME.parent().joinpath("run"), i=2)
+        run_notebook(paths.tests.joinpath("run"), i=2)
         assert False
     except Exception:
         assert True  # notebook should fail
