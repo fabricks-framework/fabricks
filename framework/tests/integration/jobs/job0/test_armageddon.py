a70dd0c17fc19f030ea661cfdd17789087fa4c0b
francois.poskin@bmsuisse.ch <francois.poskin@bmsuisse.ch>
2026-06-30 14:21:59 +0000

    minor

--- framework/tests/integration/jobs/job0/test_armageddon.py
+++ framework/tests/integration/jobs/job0/test_armageddon.py
@@ -6,4 +6,4 @@ from fabricks.context import SPARK
 @pytest.mark.order(101)
 def test_armageddon():
     df = SPARK.sql("select * from fabricks.dbojects where exists")
-    assert df.count() == 128, "armageddon should have created 128 tables and views"
+    assert df.count() == 96, "armageddon should have created 128 tables and views"
