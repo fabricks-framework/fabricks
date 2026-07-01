1ca485c34a3f0a85f3f717009dc47040d6db6448
François Poskin <francois.poskin@bmsuisse.ch>
2026-07-01 15:46:51 +0200

    fix

--- framework/tests/integration/jobs/job0/test_cdc.py
+++ framework/tests/integration/jobs/job0/test_cdc.py
@@ -4,6 +4,7 @@ import pytest
 
 from fabricks.cdc import SCD1, SCD2
 from fabricks.context import SPARK
+from fabricks.context.log import DEFAULT_LOGGER
 from fabricks.models.cdc import CdcContext
 from tests.integration.compare import assert_dfs_equal
 
@@ -13,18 +14,32 @@ CDC = {"scd1": SCD1, "scd2": SCD2}
 @pytest.mark.integration
 @pytest.mark.parametrize("cdc", ["scd1", "scd2"])
 @pytest.mark.parametrize("iter", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-def test_cdc_isolated(cdc: Literal["scd1", "scd2", "latest"], iter: int):
+def test_cdc_isolated(cdc: Literal["scd1", "scd2", "latest"], iter: int | list[int] = 1):
+    if isinstance(iter, int):
+        iter = [iter]
+
+    iter_str = [str(i) for i in iter]
+    last_iter = iter[-1]
     topic = "monarch"
-    tgt: SCD1 | SCD2 = CDC[cdc]("test", f"{topic}_{cdc}_{iter}")
-    # tgt.drop()
-    view = f"input.{topic}_job{iter}"
+    tgt: SCD1 | SCD2 = CDC[cdc]("test", f"{topic}_{cdc}_{'_'.join(iter_str)}")
+    expected = f"expected.silver_{cdc}_job{last_iter}"
+    DEFAULT_LOGGER.info(f"comparing to {expected}")
+    x = 0
 
-    if not SPARK.catalog.tableExists(view):
-        raise Exception(f"table {view} does not exist")
+    for i in iter_str:
+        view = f"input.monarch_job{i}"
+        query = f"select * from {view}"
+        context = CdcContext(keys=["id"], schema_drift=True)
 
-    query = f"select * from {view}"
-    context = CdcContext(keys=["id"])
-    tgt.create_table(query, context=context)
-    tgt.update(query, context=context)  # mirrors silver.py mode="update"
-    expected = SPARK.table(f"expected.silver_{cdc}_job{iter}").drop("__source")
-    assert_dfs_equal(tgt.table.dataframe, expected, soft_delete=False)
+        if cdc == "scd2":
+            context.correct_valid_from = True
+
+        if x == 0:
+            tgt.drop()
+            tgt.create_table(query, context=context)
+
+        tgt.update(query, context=context)  # mirrors silver.py mode="update"
+        x += 1
+
+    expected_df = SPARK.table(expected).drop("__source")
+    assert_dfs_equal(tgt.table.dataframe, expected_df, soft_delete=False)
